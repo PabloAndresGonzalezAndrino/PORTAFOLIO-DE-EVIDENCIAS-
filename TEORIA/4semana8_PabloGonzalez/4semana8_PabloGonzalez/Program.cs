42fb5cb5aa737d422297c1e2404a737e4cd13cda
PabloAndresGonzalezAndrino <118147175+PabloAndresGonzalezAndrino@users.noreply.github.com>
2022-11-16 20:40:15 -0600

    Add files via upload

---
+++ TEORIA/4semana8_PabloGonzalez/4semana8_PabloGonzalez/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{
+    static void Main()
+    {
+        /*Console.Clear();
+         */
+        string[] aPalabras = { ¨ADIOS¨, ¨Arbol¨, ¨marcador¨,¨LAPTOP¨, ¨avion¨, ¨perro¨, ¨AZUL¨,¨cuatro¨, ¨Veinte¨, ¨gato¨}
+        string nombre = ¨ejemplo variable¨;
+
+        for (int inc = 0; inc < 10, inc++)
+        {
+            Console.WriteLine(¨Validando mayusculas¨ +aPalabras[inc]);
+            if (aPalabras[inc].ToUpper() == aPalabras[inc])
+            {
+                Console.WriteLine(¨Validando mayusculas¨ +aPalabras[inc] + ¨es mayuscula. ¨);
+            }
+
+        }
+    }
+}
